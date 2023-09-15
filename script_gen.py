c4f32cdf3c51d673c4a54ada66ec6adcd4076477
caffreyu <caffreyu@gmail.com>
2023-09-15 00:20:29 -0700

    feat: add a py script to auto generate script from csv

---
+++ script_gen.py
@@ -0,0 +1,116 @@
+# TODO: use a logger to record the messages printed on the terminal screen
+
+import csv
+import sys
+import os
+import glob
+
+CHAR_DICT = {
+    "时茜": "qian",
+    "时茜：": "qian_speaking",
+    "黑衣女人": "bfem",
+    "黑衣女人：": "bfem_speaking",
+    "衬衫女人：": "off_speaking",
+    "瘦小女人：": "thin_speaking",
+    "短发女人：": "short_speaking",
+    "工作人员：": "stf_speaking",
+    "老板：": "boss_speaking",
+    "君和：": "jun_speaking",
+    "迟玉：": "chi_speaking",
+    "莺莺：": "ying_speaking",
+    "黎沙：": "li_speaking",
+    "？": "unknown",
+    "？：": "unknown",
+}
+JUMP_IDENTIFIER = "Q"
+FNAME_IDENTIFIER = "Q"
+
+
+def get_digits(
+    s: str,
+):
+    return "_".join([ele for ele in s if ele.isdigit()])
+
+
+def gen_txt(
+    fname: str,
+    digits: str,
+    output_fname: str = "output.txt",
+):
+    # Process input file
+    data = []
+    with open(fname, "r") as file:
+        csv_reader = csv.reader(file)
+
+        for row in csv_reader:
+            data.append(row)
+
+    # Remove and create a new file with output_fname
+    file = open(output_fname, "w")
+    file.close()
+
+    # Write to output file output_fname
+    # digits = [ele for ele in fname if ele.isdigit()]
+    label_name = f"label q{digits}:\n"
+
+    line_counter = 0
+    menu_flag = False
+    with open(output_fname, "a") as file:
+        file.write(label_name)
+        for person, dialog, note in data:
+            line_counter += 1
+            # Skip if dialog is empty
+            if len(dialog) == 0:
+                continue
+            # Check if person in CHAR_DICT
+            if len(person) > 0 and person not in CHAR_DICT:
+                print(
+                    f"[ERROR] Undefined character at {fname} row {line_counter} column 1.\n"
+                )
+            # Character with dialog
+            if person in CHAR_DICT:
+                menu_flag = False
+                file.write(f'\t{CHAR_DICT[person]} "{dialog}"\n')
+                # With notes
+                if len(note) > 0:
+                    file.write(f"\t#TODO: {note}\n")
+            # Only dialog
+            elif len(dialog) > 0 and JUMP_IDENTIFIER not in dialog:
+                menu_flag = False
+                file.write(f'\t"{dialog}"\n')
+            # Condition and jump
+            else:
+                try:
+                    condition, chapter = dialog.split("Q")
+                    if not menu_flag:
+                        file.write("\tmenu:\n")
+                        menu_flag = True
+
+                    file.write(f'\t\t"{condition}":\n')
+                    file.write(f"\t\t\tjump q{digits}\n")
+                except:
+                    print(
+                        (
+                            f"[ERROR] Format of content at {fname} row {line_counter} column 2 is off.\n"
+                            + r"Expect <ACTION_NAME>Q\d+(\.\d+)*"
+                        )
+                    )
+
+    data.clear()
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) >= 2
+    input_file_dir = sys.argv[1]
+    input_file_dir = os.path.expanduser(input_file_dir)
+    input_file_list = glob.glob(f"{input_file_dir}/*.csv")
+
+    for input_fname in input_file_list:
+        print(f"[INFO] Processing {input_fname}")
+        assert ".csv" in input_fname and input_fname.count(".csv") == 1
+        input_fname_list = input_fname.split(FNAME_IDENTIFIER)
+        digits = "_".join([ele for ele in input_fname_list[1] if ele.isdigit()])
+
+        output_dir = os.path.join(os.getcwd(), "game/tmp_scripts")
+        output_fname = os.path.join(output_dir, f"script_{digits}.rpy")
+        gen_txt(fname=input_fname, digits=digits, output_fname=output_fname)
